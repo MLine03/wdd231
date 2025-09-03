41cbc8b24c1eaee031a74ef211cfed651a2e3e53
Meline Levao <your-github-email@example.com>
2025-09-03 14:31:41 -0600

    Fix folder structure and file references for no 404 errors

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-// Set copyright year and last modified date
-document.getElementById('year').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = document.lastModified;
-
-// Example code to dynamically add courses or other interactions can be added here
+// Sample JS - No console.logs, no onclick attributes
+document.addEventListener('DOMContentLoaded', () => {
+    // Add event listeners or JS code here if needed
+  });
+  
